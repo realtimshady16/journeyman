d060ad27c1f3e12cb594eb6a3b1c7d134fdf4316
vboxuser <vboxuser@Deskop.notemail>
2025-04-25 14:52:02 +0000

    I need to get better at these

---
+++ networking/server.py
@@ -0,0 +1,10 @@
+import socket
+
+def start_server():
+    host = "127.0.0.1"
+    port = 12345
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+
+    server_socket =
